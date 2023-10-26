bac834a1ead0f8b2603d508496cb5b5379176ce6
Jamie Holland <Jamie.Holland@imi-critical.com>
2023-10-26 13:30:06 +0100

    Update versioning.sh

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -6,7 +6,8 @@ VERSION=`git describe --abbrev=0 --tags`
 #get number parts and increase last one by 1
 VNUM1=$(echo "$VERSION" | cut -d"." -f1)
 VNUM2=$(echo "$VERSION" | cut -d"." -f2)
-
+VNUM3=$(echo "$VERSION" | cut -d"." -f3)
+VNUM4=$(echo "$VERSION" | cut -d"." -f4)
 VNUM1=`echo $VNUM1 | sed 's/v//'`
 
 # Check for #major or #minor in commit message and increment the relevant version number
@@ -20,26 +21,30 @@ if [ "$MAJOR" ]; then
     VNUM1=$((VNUM1+1))
     VNUM2=0
     VNUM3=0
+    VNUM4=0
 elif [ "$MINOR" ]; then
     echo "Update minor version"
     VNUM2=$((VNUM2+1))
     VNUM3=0
+    VNUM4=0
 elif [ "$PATCH" ]; then
     echo "Update patch version"
     VNUM3=$((VNUM3+1))
+    VNUM4=0
 else
+    VNUM4=0
     echo "No instruction detected a tag wont be added to this commit"
 fi
 
-# check for release
+#create new tag
+NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
+
+# check for release candidate
 if [ "$RELCAN" ]; then
     echo "Update release candidate version"
     VNUM4=$((VNUM4+1))
     NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-rc.$VNUM4"
 fi
-
-#create new tag
-NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
 
 #get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
