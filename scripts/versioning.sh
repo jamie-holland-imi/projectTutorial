8b5a21bc9e253b4f0dff3652c721bfea17f8464c
Jamie Holland <Jamie.Holland@imi-critical.com>
2023-10-26 13:37:11 +0100

    Update versioning.sh patch

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -52,7 +52,7 @@ NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 echo "###############################################################"  
 #only tag if no tag already (would be better if the git describe command above could have a silent option)
-if [ -z "$NEEDS_TAG" && !$(git tag -l "$NEW_TAG") ]; then
+if [ -z "$NEEDS_TAG" && !($(git tag -l "$NEW_TAG")) ]; then
     echo "Updating $VERSION to $NEW_TAG"
 #    echo "Tagged with $NEW_TAG (Ignoring fatal:cannot describe - this means commit is untagged) "
     git tag $NEW_TAG
