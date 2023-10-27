69ab36c5b6ad78dbe3a910b0ca6f9583686ceaf8
Jamie Holland <Jamie.Holland@imi-critical.com>
2023-10-27 10:36:29 +0100

    Update versioning.sh (MAJORRC)

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -44,7 +44,7 @@ if [ "$RC" ]; then
     VNUM5=$((VNUM5+1))
     NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VCHAR.$VNUM5"
 elif [ "$MAJORRC" ]; then
-    if [ "$VNUM2" == 0 ] && [ "$VNUM3" == 0 ] && [ "$VNUM4" == "rc" ]; then
+    if [ "$VNUM4" == "rc" ]; then
         echo "Going to RC as currently already a major version"
         VNUM5=$((VNUM5+1))
     elif [ "$VNUM4" == "rc" ]; then
