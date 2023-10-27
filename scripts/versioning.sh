8fa1f31bafd6626ebb0c9b231cfc6f67a46c038f
Jamie Holland <Jamie.Holland@imi-critical.com>
2023-10-27 11:53:41 +0100

    Update versioning.sh (MAJORRC)

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -7,9 +7,9 @@ OLDVERSION=$VERSION
 #get number parts and increase last one by 1
 VNUM1=$(echo "$VERSION" | cut -d"." -f1)
 VNUM2=$(echo "$VERSION" | cut -d"." -f2)
-VNUM3=$(echo "$VERSION" | cut -d"." -f3)
-VNUM4=$(echo "$VERSION" | cut -c"-" -f3)
-VNUM5=$(echo "$VERSION" | cut -d"." -f5)
+VNUM3=$(echo "$VERSION" | cut -c 6)
+VNUM4=$(echo "$VERSION" | cut -c 8-9)
+VNUM5=$(echo "$VERSION" | cut -c 11)
 VNUM1=`echo $VNUM1 | sed 's/v//'`
 
 echo "**************"
