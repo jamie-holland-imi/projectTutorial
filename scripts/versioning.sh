ad2615328484f68067926947649fac81e1876d37
Jamie Holland <Jamie.Holland@imi-critical.com>
2023-10-30 10:34:42 +0000

    Update versioning.sh

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -10,12 +10,15 @@ VNUM2=$(echo "$VERSION" | cut -d"." -f2)
 VNUM3=$(echo "$VERSION" | cut -d"-" -f3 | cut -d"." -f4)
 VNUM4=$(echo "$VERSION" | cut -d"." -f5 | cut -d"-" -f6)
 VNUM5=$(echo "$VERSION" | cut -d"." -f7)
-VNUM1=`echo $VNUM1 | sed 's/v//'`
+VNUM1=`echo $VNUM1 | sed 's/V//'`
 
 # Check for #major or #minor in commit message and increment the relevant version number
+RELEASE=`git log --format=%B -n 1 HEAD | grep '(RELEASE)'`
 MAJOR=`git log --format=%B -n 1 HEAD | grep '(MAJOR)'`
 MINOR=`git log --format=%B -n 1 HEAD | grep '(MINOR)'`
 PATCH=`git log --format=%B -n 1 HEAD | grep '(PATCH)'`
+ALPHA=`git log --format=%B -n 1 HEAD | grep '(ALPHA)'`
+BETA=`git log --format=%B -n 1 HEAD | grep '(BETA)'`
 RC=`git log --format=%B -n 1 HEAD | grep '(RC)'`
 MAJORRC=`git log --format=%B -n 1 HEAD | grep '(MAJORRC)'`
 
@@ -25,57 +28,70 @@ echo "3 $VNUM3"
 echo "4 $VNUM4"
 echo "5 $VNUM5"
 
-if [ "$MAJOR" ]; then
+if [ "$RELEASE" ]; then
+    NEW_TAG="V$VNUM1.$VNUM2.$VNUM3"
+elif [ "$MAJOR" ]; then
     echo "Update major version"
     VNUM1=$((VNUM1+1))
     VNUM2=0
     VNUM3=0
-    NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
+    VNUM5=1
+    NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
 elif [ "$MINOR" ]; then
     echo "Update minor version"
     VNUM2=$((VNUM2+1))
     VNUM3=0
-    NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
+    VNUM5=1
+    NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
 elif [ "$PATCH" ]; then
     echo "Update patch version"
     VNUM3=$((VNUM3+1))
-    NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
+    VNUM5=1
+    NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+elif [ "$ALPHA" ]; then
+    if [ "$VNUM4" == 'alpha' ]; then
+        echo "Update alpha version"
+        VNUM5=$((VNUM5+1))
+        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+    else
+        echo "Set alpha version"
+        VNUM4='alpha'
+        VNUM5=1
+        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+    fi
+elif [ "$BETA" ]; then
+    if [ "$VNUM4" == 'beta' ]; then
+        echo "Update beta version"
+        VNUM5=$((VNUM5+1))
+        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+    else
+        echo "Set beta version"
+        VNUM4='beta'
+        VNUM5=1
+        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+    fi
 elif [ "$RC" ]; then
-    if [ -z "$VNUM5" ]; then
+    if [ "$VNUM4" == 'rc' ]; then
+        echo "Update release candidate"
+        VNUM5=$((VNUM5+1))
+        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+    else
         echo "For current tag set release candidate"
-        VNUM1=$((VNUM1+1))
-        VNUM2=0
-        VNUM3=0
         VNUM4='rc'
         VNUM5=1
-        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
-    else
-        echo "Update release candidate version"
-        VNUM5=$((VNUM5+1))
-        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     fi
 elif [ "$MAJORRC" ]; then
-    if [ "$VNUM5" >= '1' ] && [ "$VNUM4" == 'rc' ]; then
-        echo "Already RC will instead just increment it"
-        VNUM5=$((VNUM5+1))
-        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
-    elif [ "$VNUM2" == '0' ] && [ "$VNUM3" == '0' ]; then
-        echo "Setting RC as currently already a major version"
-        VNUM4='rc'
-        VNUM5=1
-        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
-    else
-        echo "Update major and set release candidate"
-        VNUM1=$((VNUM1+1))
-        VNUM2=0
-        VNUM3=0
-        VNUM4='rc'
-        VNUM5=1
-        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
-    fi
+    echo "Update major and set release candidate"
+    VNUM1=$((VNUM1+1))
+    VNUM2=0
+    VNUM3=0
+    VNUM4='rc'
+    VNUM5=1
+    NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
 elif [ -z "$VERSION" ]; then
-    echo "No tag exists setting the first tag to 0.0.0"
-    NEW_TAG="v0.0.0"
+    echo "No tag exists setting the first tag to V0.0.0"
+    NEW_TAG="V0.0.0"
 else
     NEW_TAG="nochange"
 fi
