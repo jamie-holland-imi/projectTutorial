3479133ff89a023ec04cc4331be4af6d444b1be9
Jamie Holland <Jamie.Holland@imi-critical.com>
2023-10-31 16:46:43 +0000

    Update versioning.sh (PATCH)

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #get highest tag number
-VERSION=`git describe --abbrev=0 --tags`
+VERSION=`git describe --tags --abbrev=0 `git rev-list --tags --max-count=1``
 BRANCH=`git branch --show-current`
 
 #get number parts of the current tag
