bf38a236d926e18fb78459255c8961e6610b4f75
Jamie Holland <Jamie.Holland@imi-critical.com>
2023-11-02 15:32:24 +0000

    Update versioning.sh

--- scripts/versioning.sh
+++ scripts/versioning.sh
@@ -11,7 +11,7 @@ VNUM2=$(echo "$VERSION" | cut -d"." -f2)
 VNUM3=$(echo "$VERSION" | cut -d"." -f3 | grep -Eo '[0-9]+')
 VNUM4=$(echo "$VERSION" | cut -d"." -f3 | grep -Eo '[[:alpha:]]+')
 VNUM5=$(echo "$VERSION" | cut -d"." -f4)
-VNUM1=`echo $VNUM1 | sed 's/V//'`
+VNUM1=`echo $VNUM1 | sed 's/v//'`
 
 # Check for #major or #minor in commit message and increment the relevant version number
 MAJOR=`git log --format=%B -n 1 HEAD | grep '(MAJOR)'`
@@ -31,12 +31,13 @@ if [ -z "$VERSION" ]; then
     VNUM3=0
     VNUM4='alpha'
     VNUM5=1
-    NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+    NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
 # check if the branch is not main then tag must drop down to beta
 elif ([ "$VNUM4" == 'rc' ] && [ "$BRANCH" != "main" ]); then
     VNUM4='beta'
-    VNUM5=0
-    NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+    VNUM5=1
+    NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+    VALID=false
 fi
 
 if [ "$MAJOR" ]; then
@@ -45,36 +46,36 @@ if [ "$MAJOR" ]; then
     VNUM2=0
     VNUM3=0
     if [ -z "$VNUM4" ]; then
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
     else
         VNUM5=0
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     fi
 elif [ "$MINOR" ]; then
     echo "Update minor version"
     VNUM2=$((VNUM2+1))
     VNUM3=0
     if [ -z "$VNUM4" ]; then
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
     else
         VNUM5=0
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     fi
 elif [ "$PATCH" ]; then
     echo "Update patch version"
     VNUM3=$((VNUM3+1))
     if [ -z "$VNUM4" ]; then
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
     else
         VNUM5=0
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     fi
 fi
 
 if [ "$CLEAN" ]; then
     if [ "$BRANCH" == "main" ]; then
         echo "Create a clean release tag removing additional labels"
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3"
     else
         echo "Must be on the main branch to create a clean release tag"
         NEW_TAG="invalidbranch"
@@ -83,43 +84,46 @@ elif [ "$ALPHA" ]; then
     if [ "$VNUM4" == 'alpha' ]; then
         echo "Update alpha version"
         VNUM5=$((VNUM5+1))
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     else
         echo "Set alpha version"
         VNUM4='alpha'
         VNUM5=1
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     fi
 elif [ "$BETA" ]; then
     if [ "$VNUM4" == 'beta' ]; then
         echo "Update beta version"
         VNUM5=$((VNUM5+1))
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     else
         echo "Set beta version"
         VNUM4='beta'
         VNUM5=1
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     fi
 elif [ "$PHASE" ]; then
     if [ -z "$VNUM4" ]; then
-        echo "Not currently in a phase no change to be done"
+        echo "Not currently in a phase will set to alpha"
+        VNUM4='alpha'
+        VNUM5=1
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     else
         echo "Update phase $VNUM4 version"
         VNUM5=$((VNUM5+1))
-        NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
     fi
 elif [ "$RC" ]; then
     if [ "$BRANCH" == "main" ]; then
         if [ "$VNUM4" == 'rc' ]; then
             echo "Update release candidate"
             VNUM5=$((VNUM5+1))
-            NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+            NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
         else
             echo "For current tag set release candidate"
             VNUM4='rc'
             VNUM5=1
-            NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+            NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
         fi
     else
         echo "Must be on the main branch to create a RC tag"
@@ -127,7 +131,21 @@ elif [ "$RC" ]; then
     fi
 elif [ "$VNUM5" == "0" ]; then
     VNUM5=1
-    NEW_TAG="V$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+    NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+fi
+
+if [ $(git tag -l "$NEW_TAG") ]; then
+    VALID=false
+    while [ "$VALID" = false ];  do
+        if [ $(git tag -l "$NEW_TAG") ]; then
+            echo "The Tag $NEW_TAG already exists incrementing PHASE by 1"
+            VNUM5=$((VNUM5+1))
+            NEW_TAG="v$VNUM1.$VNUM2.$VNUM3-$VNUM4.$VNUM5"
+        else
+            echo "valid Tag setting tag to $NEW_TAG"
+            VALID=true
+        fi
+    done
 fi
 
 #get current hash and see if it already has a tag
